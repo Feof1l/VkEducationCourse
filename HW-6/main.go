afa8fc105ff3ed1c600b12d88fdcb5b52ca8c344
feof1l <ivan.vasilov.03@mail.ru>
2024-01-21 01:08:28 +0300

    6th HW

--- HW-6/main.go
+++ HW-6/main.go
@@ -5,12 +5,16 @@ import (
 )
 
 func main() {
-	var s string
+	/*var s string
 	fmt.Scanln(&s)
-	//fmt.Println(string(s))
-	//r := []rune(s)
-	//fmt.Println(string(r))
-	fmt.Println(string(deleteDouble(s)))
+	fmt.Println(string(deleteDouble(s)))*/
+	var n int
+	fmt.Scanln(&n)
+	mas := make([]int, n, n)
+	for i, _ := range mas {
+		fmt.Scan(&mas[i])
+	}
+	fmt.Println(stack(n, mas))
 
 }
 func deleteDouble(s string) string {
@@ -42,3 +46,21 @@ func deleteDouble(s string) string {
 //Дана строка s. Строка состоит из английских букв в нижнем регистре.Необходимо из строки удалить все рядом стоящие повторяющиеся буквы.
 //Например, в строке xyyx мы должны удалить yy, а после и оставшиеся xx и того после должна получиться пустая строка. Или же в строке fqffqzzsd после удаления остануться только fsd. Первыми удаляться ff,
 //являющимися третьими и четвертыми символами, затем qq и после уже zz.
+
+func stack(n int, mas []int) int {
+	s := make([]int, 0)
+	for _, v := range mas {
+		if v%2 == 0 {
+			s = append(s, v)
+		}
+	}
+	if len(s) != 0 {
+		return s[len(s)-1]
+	} else {
+		return -1
+	}
+
+}
+
+//Дан массив не отсортированных целых чисел. Написать функцию, которая вернет первое с конца четное число.
+//При написании кода используйте принцип стека. Если массив не содержит четного числа возвращать -1.

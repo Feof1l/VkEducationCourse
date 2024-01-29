087241fe670d86771473325642d14f341bb3e49a
feof1l <ivan.vasilov.03@mail.ru>
2024-01-29 13:00:02 +0300

    task 7

---
+++ HW-7/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+	mas := make([]int, n, n)
+	for i, _ := range mas {
+		fmt.Scan(&mas[i])
+	}
+	var p int
+	fmt.Scanln(&p)
+	//fmt.Println(n, p, mas)
+	fmt.Println(BinarySeach(mas, p))
+
+}
+func BinarySeach(mas []int, p int) bool {
+	l := 0
+	r := len(mas) - 1
+	for l <= r {
+		mid := l + (r-l)/2
+		if mas[mid] == p {
+			return true
+		} else if mas[mid] > p {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return false
+}
